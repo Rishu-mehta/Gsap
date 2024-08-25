25bf2cacffa00d641cbc50c0dcc5d724fe33be23
Rishu mehta <rishumehta7370@gmail.com>
2024-08-25 23:59:50 +0530

    created navigation bar with timeline and to,from

---
+++ timeline,to,from/gsap.js
@@ -0,0 +1,22 @@
+var tl = gsap.timeline()
+
+tl.from(".brand h1",{
+    y:-30,
+    duration:1,
+    dalay:1,
+    opacity:0
+
+})
+tl.from(".navigations ul li",{
+    y:-30,
+    duration:1,
+    opacity:0,
+    stagger:0.2
+
+})
+tl.from(".hero-section h1",{
+    y:20,
+    opacity:0,  
+    scale:0.2,
+    duration:2
+})
